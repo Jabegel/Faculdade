fce917ed64d1ef994387d9e75e4c872c114c4b78
João Gabriel <joaogabriel.dos.s.pereira@gmail.com>
2024-09-05 23:15:18 -0300

    Aula 04

---
+++ Aula 04/Variavel Exemplo 01.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main() {
+int idade = 25; // Variável inteira
+float altura = 1.75; // Variável de ponto flutuante
+char inicial = 'A'; // Variável de caractere
+printf("Idade: %d\n", idade);
+printf("Altura: %.2f\n", altura);
+printf("Inicial do nome: %c\n", inicial);
+return 0;
+}
